34e3da30179203f83583164c252c37ed07baae8f
Noémi SALAÜN <noemi.salaun@gmail.com>
2012-12-07 22:37:30 +0100

    Amélioration du générateur aléatoire

--- java/src/patternMatching/RandomStringGenerator.java
+++ java/src/patternMatching/RandomStringGenerator.java
@@ -3,37 +3,56 @@ package patternMatching;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomStringGenerator {
-	
-	private String filePath;
-	private int patternLength;
-	private int alphabetSize;
-	
-	public RandomStringGenerator(int patternLength, int alphabetSize, String fileName) {
-		this.filePath = System.getProperty("user.dir") + "/"+ fileName;
-		this.patternLength = patternLength;
-		this.alphabetSize = alphabetSize;
-	}
-	
-	public String generate() {
-		String ret = "";
-		String text = "";
-		Random random = new Random();
-		try {
-			FileWriter fw = new FileWriter(filePath, false);
-			BufferedWriter output = new BufferedWriter(fw);
-			for (int i=0 ; i<patternLength ; i++) {
-				text = String.valueOf(random.nextInt(alphabetSize));
-				text = (i==patternLength-1) ? text : text + " ";
-				output.write(text);
-			}
-			output.close();
-			ret = "Success !";
-		} catch(IOException ioe) {
-			ret = "Error : " + ioe.getMessage();
+
+	private ArrayList<Integer> alphabet;
+
+	public RandomStringGenerator(int alphabetSize) {
+		alphabet = new ArrayList<>();
+		for (int token = 0; token < alphabetSize; token++) {
+			alphabet.add(token);
 		}
-		return ret;
+	}
+
+	public ArrayList<Integer> generate(int textLength, String filePath) throws IOException {
+		int alphabetSize = getAlphabet().size();
+		ArrayList<Integer> text = new ArrayList<>();
+		Random random = new Random();
+		int token;
+		if (filePath == null) {
+			for (int i = 0; i < textLength; i++) {
+				token = random.nextInt(alphabetSize);
+				text.add(token);
+			}
+		} else {
+			String stringToken;
+			FileWriter fw = new FileWriter(System.getProperty("user.dir") + "/" + filePath, false);
+			try (BufferedWriter output = new BufferedWriter(fw)) {
+				for (int i = 0; i < textLength; i++) {
+					token = random.nextInt(alphabetSize);
+					text.add(token);
+					stringToken = (i == textLength - 1) ? String.valueOf(token) : token + " ";
+					output.write(stringToken);
+				}
+			}
+		}
+		return text;
+	}
+
+	public ArrayList<Integer> generate(int textLength) {
+		ArrayList<Integer> text = new ArrayList<>();
+		try {
+			text = generate(textLength, null);
+		} catch (IOException ex) {
+			// L'exception ne peut pas être déclanché si on ne demande pas l'écriture dans un fichier
+		}
+		return text;
+	}
+
+	public ArrayList<Integer> getAlphabet() {
+		return alphabet;
 	}
 }
